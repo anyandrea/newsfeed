a7c8e7351990e3d3b247de2510ca0a3752c080dc
anyandrea <aaymon@gmx.net>
2018-09-08 15:34:30 +0200

    format feed view

--- lib/web/render.go
+++ lib/web/render.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"html/template"
+
 	"github.com/unrolled/render"
 )
 
@@ -9,10 +11,23 @@ var r *render.Render
 func init() {
 	// setup template rendering
 	r = render.New(render.Options{
+		Directory:  "templates",
 		Layout:     "layout",
 		Extensions: []string{".tmpl", ".html"},
+		Funcs: []template.FuncMap{template.FuncMap{
+			"isEvenNumber": isEvenNumber,
+			"isOddNumber":  isOddNumber,
+		}},
 		IndentJSON: true,
 	})
+}
+
+func isEvenNumber(input int) bool {
+	return input%2 == 0
+}
+
+func isOddNumber(input int) bool {
+	return !isEvenNumber(input)
 }
 
 func Render() *render.Render {
